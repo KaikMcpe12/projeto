998689faefc98525e1b5225a50782d8f399ae82a
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2025-02-04 14:13:33 -0300

    :sparkles: feat: added user repository

--- src/routes/create-user.ts
+++ src/routes/create-user.ts
@@ -2,27 +2,25 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { PrismaUserRepository } from "../prisma/repository/user-repository";
+import createUserController from "../controller/create-user-controller";
 
 export async function createUser(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post("/user",
         {
             schema: {
                 body: z.object({
-                    nome: z.string(),
+                    name: z.string(),
                     email: z.string()
                 })
             }
         },
         async (request) => {
-            const { nome, email } = request.body
+            const { name, email } = request.body
 
-            const user = await prisma.user.create({
-                data: {
-                    name: nome,
-                    email: email
-                }
-            })
+            const prismaClient = new PrismaUserRepository(prisma)
+            const userId = await createUserController(prismaClient, { name, email})
 
-            return { userId: user.id }
+            return { userId }
         })
 }
