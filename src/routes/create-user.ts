d43aa0cff0509b639cafcd205c9fc65fbfe535cd
Kaik <119709479+KaikMcpe12@users.noreply.github.com>
2025-02-04 18:49:00 -0300

    ⏪ fix: reverting changes

--- src/routes/create-user.ts
+++ src/routes/create-user.ts
@@ -2,25 +2,27 @@ import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
-import { PrismaUserRepository } from "../prisma/repository/user-repository";
-import createUserController from "../controller/create-user-controller";
 
 export async function createUser(app: FastifyInstance) {
     app.withTypeProvider<ZodTypeProvider>().post("/user",
         {
             schema: {
                 body: z.object({
-                    name: z.string(),
+                    nome: z.string(),
                     email: z.string()
                 })
             }
         },
         async (request) => {
-            const { name, email } = request.body
+            const { nome, email } = request.body
 
-            const prismaClient = new PrismaUserRepository(prisma)
-            const userId = await createUserController(prismaClient, { name, email})
+            const user = await prisma.user.create({
+                data: {
+                    name: nome,
+                    email: email
+                }
+            })
 
-            return { userId }
+            return { userId: user.id }
         })
 }
